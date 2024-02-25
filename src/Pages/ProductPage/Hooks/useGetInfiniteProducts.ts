f6169362a635f66824e34e066fe359c850bd8aed
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2024-02-25 07:28:33 +0100

    Fix(): Fixed scrolling not stopping even if hasMore is false

--- src/Pages/ProductPage/Hooks/useGetInfiniteProducts.ts
+++ src/Pages/ProductPage/Hooks/useGetInfiniteProducts.ts
@@ -14,7 +14,12 @@ function useGetInfiniteProducts() {
             initialPageParam: 0,
             queryKey: [ProductPageApiQueryKeys.PRODUCTS],
             queryFn: ({ pageParam }) => fetchProducts({ pageParam }),
-            getNextPageParam: (lastPage) => lastPage.pageParam + 1,
+            getNextPageParam: (lastPage) => {
+                if (lastPage.hasMore) {
+                    return lastPage.pageParam + 1
+                }
+                return undefined
+            },
         })
     const allProducts = data?.pages.flatMap((page) => page.products) || []
 
