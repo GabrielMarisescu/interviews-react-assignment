6c8b0af1c49b0b38670682c7bae44c1e84302c88
GabrielMarisescu <gabriel.lavoro10@gmail.com>
2024-03-08 15:44:49 +0100

    Refactor(): Switched from Cypress to Vitest,deleted cypress configs, added vitest config& changed package.json

--- cypress/e2e/ProductsPage.cy.tsx
+++
@@ -1,8 +0,0 @@
-describe('Mounts the FreshCard Market App', () => {
-    //No Need to add a mock since we are already mocking it in the main app.
-    //otherwise I would create a mock for the products.
-    beforeEach(() => {
-        cy.visit('http://localhost:5173/')
-    })
-    it('should display the products', () => {})
-})
